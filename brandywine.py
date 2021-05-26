e98ca81c23fc30ec2dac767b9ada24a49480c500
Gavin Faulkner <82196920+gtfmadrigal@users.noreply.github.com>
2021-05-25 23:07:05 -0400

    v2.1.2

--- brandywine.py
+++ brandywine.py
@@ -17,13 +17,15 @@ testVariable = 0
 # Unit dictionaries
 healthTable = {"infantry":4, "sapper":4, "fusilier":6, "grenadier":8, "bombadier":10, "spy":10, "hussar":12, "dragoon":16, "special":20, "command":20}
 movementTable = {"bombadier":5, "dragoon":5, "spy":5, "command":5, "infantry":10, "sapper":10, "grenadier":10, "hussar":10, "special":10, "fusilier":15}
-headingChange = []
 attackTable = {"infantry":4, "sapper":4, "fusilier":4, "grenadier":4, "bombadier":4, "spy":4, "hussar":12, "dragoon":20, "special":20, "command":20}
 fireTable = {"grenadier":8, "bombadier":10, "hussar":12, "dragoon":20}
 buildTable = {"infantry":4, "sapper":8}
+
+# Unit lists
 searchable = ["infantry", "sapper", "fusilier"]
 hideable = ["infantry", "sapper", "fusilier", "grenadier", "spy", "special"]
 moveAndFire = ["infantry", "sapper", "fusilier", "hussar", "dragoon", "special"]
+headingChange = []
 
 # info() function
 def info(unitType):
