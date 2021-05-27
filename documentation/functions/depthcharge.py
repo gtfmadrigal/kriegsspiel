3d2368bf516be6e98f7e4dac9c8c7fd880f4d77d
Gavin Faulkner <82196920+gtfmadrigal@users.noreply.github.com>
2021-05-27 00:40:27 -0400

    v2.2.10

--- documentation/functions/depthcharge.py
+++ documentation/functions/depthcharge.py
@@ -8,25 +8,31 @@ def depthcharge(unit, unitType, team): # depthcharge() takes three arguments: un
     global immobileUnits # Str list, contains units that have already moved this turn.
     global usedUnits # Str list, contains units that have already taken some combat action this turn.
     global alreadyDropped # Str list, contains units that have already dropped depth charges this turn.
-    
-    global firstDamage
-    global secondDamage
-    if not unitType in depthchargeable:
-        print("Cannot drop depth charges.")
-        return
-    if unit in alreadyDropped:
-        print("Already dropped depth charges.")
-        return
-    chargeEffectiveness = random.randrange(1,7)
-    if chargeEffectiveness == 6:
-        print("Submarine sunk.")
-        if team == firstTeam: firstDamage = firstDamage + 1
+    global firstDamage # Int variable, contains the amount of damage the first team has inflicted up to that point in the game.
+    global secondDamage # Same as firstDamage, but for the second team.
+
+    # Checks
+    if not unitType in depthchargeable: # If the unit type is not able to drop a depth charge ...
+        print("Cannot drop depth charges.") # ... an error message is displayed, and ...
+        return # the function returns to the shell without any action being taken.
+
+    if unit in alreadyDropped: # If the unit has already dropped a depth charge this turn ...
+        print("Already dropped depth charges.") # ... an error message is displayed, and ...
+        return # the function returns to the shell without any action being taken.
+
+    # Actual function
+    # Unlike other functions, depthcharge() does not need to look up a maximum in a dictionary, since the maximum is hard-coded at 6
+    chargeEffectiveness = random.randrange(1,7) # Note: the maximum for the random.randrange() here is 7, because the Python random.randrange function does not include the upper bound. 7 is never returned.
+    if chargeEffectiveness == 6: # Action is only taken if the chargeEffectiveness is 5 or 6. If the chargeEffectiveness is 6 ...
+        print("Submarine sunk.") # ... the target submarine is sunk, and a message to that effect displayed to the user ... 
+        if team == firstTeam: firstDamage = firstDamage + 1 # ... the damage is added to the first or second team's damage, depending on the team value passed to depthcharge(), the damage only being 1 since that is the HP ascribed to a submarine, and ... 
         elif team == secondTeam: secondDamage = secondDamage + 1
-        manual("kill")
-    elif chargeEffectiveness == 5:
-        print("Submarine disabled.")
-        disabled = input("Submarine unit disabled: ")
-        immobileUnits.append(disabled)
-        usedUnits.append(disabled)
-    immobileUnits.append(unit)
-    alreadyDropped.append(unit)
+        manual("kill") # ... manual(kill) is called to kill the submarine.
+
+    elif chargeEffectiveness == 5: # If the chargeEffectiveness is 5 ...
+        print("Submarine disabled.") # ... the target submarine is disabled, and a message to that effect displayed to the user ...
+        disabled = input("Submarine unit disabled: ") # ... the target submarine's name input by the user ...
+        immobileUnits.append(disabled) # ... the target submarine is rendered immobile that turn, and ...
+        usedUnits.append(disabled) # ... is also considered used for the turn.
+    immobileUnits.append(unit) # The ship dropping depth charges is rendered immobile.
+    alreadyDropped.append(unit) # The ship dropping depth charges is prevented from dropping another that turn, but not from taking another battle action.
