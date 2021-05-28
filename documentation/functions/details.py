d937c3d97578e7cd548e0adc0473e303a63d3c63
Gavin Faulkner <82196920+gtfmadrigal@users.noreply.github.com>
2021-05-27 22:33:47 -0400

    v2.2.11

--- documentation/functions/details.py
+++ documentation/functions/details.py
@@ -1,2 +1,5 @@
-def details():
-    print(secrets)
+# details() is a simple game function that takes no arguments. It is for the use by the empire only.
+# This function is callable from the shell with the command "details.""
+
+def details(): 
+    print(secrets) # secrets is a string variable that contains the locations of all hidden units. details() prints this variable and ends.
