f4f07bb27b2359278e58c613fc753f2c5404bc2a
Gavin Faulkner <82196920+gtfmadrigal@users.noreply.github.com>
2021-06-03 18:23:48 -0400

    v2.2.66

--- documentation/annotations/oneword/quitGame.py
+++
@@ -1,5 +0,0 @@
-# quitGame() is a function that ends the game. It is callable from the shell using the one-word command "quit".
-
-def quitGame(): # No arguments are taken.
-    score() # score() is called.
-    quit() # The Python function quit() is called, ending the program.
