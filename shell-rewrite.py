17bc86ea0ed26a008de779962a35d8c76837f98d
Gavin Faulkner <82196920+gtfmadrigal@users.noreply.github.com>
2021-06-21 00:31:08 -0400

    v3.0.2

--- shell-rewrite.py
+++ shell-rewrite.py
@@ -33,7 +33,7 @@ secondTeamFlying = []
 ships = []
 
 # Dictionaries
-errorMessages = {"team":"That unit belongs to the wrong team.", "available":"That unit is currently unavailable.", "function":"That function is unavailable to this unit.", "heading":"Unit cannot exceed its maximum heading change.", "dead":"Unit is dead.", "type":"No such unit type.", "unit":"No such unit.", "hidden":"Unit is already hidden.", "required":"Heading changes are not required for this unit.", "airborne":"Unit is not airborne.", "board":"Unit is not a boardable ship", "exists":"A unit with that name already exists."}
+errorMessages = {"argument":"Bad argument for command. Type 'man [command] for details.", "team":"That unit belongs to the wrong team.", "available":"That unit is currently unavailable.", "function":"That function is unavailable to this unit.", "heading":"Unit cannot exceed its maximum heading change.", "dead":"Unit is dead.", "type":"No such unit type.", "unit":"No such unit.", "hidden":"Unit is already hidden.", "required":"Heading changes are not required for this unit.", "airborne":"Unit is not airborne.", "board":"Unit is not a boardable ship", "exists":"A unit with that name already exists."}
 dividedTable = {}
 healthTable = {"infantry":4, "engineers":4, "mechanized":6, "light-artillery":8, "med-artillery":9, "heavy-artillery":10, "light-cavalry":12, "med-cavalry":14, "heavy-cavalry":16, "special":20, "corvette":4, "amphibious":4, "patrol":2, "cruiser":10, "destroyer":8, "battleship":12, "carrier":16, "attack-submarine":1, "missile-submarine":1, "light-fighter":4, "heavy-fighter":8, "bomber":12, "stealth-bomber":10, "recon":4, "transport":12, "drone":4}
 movementTable = {"infantry":10, "engineers":10, "mechanized":15, "light-artillery":10, "med-artillery":7, "heavy-artillery":5, "light-cavalry":10, "med-cavalry":7, "heavy-cavalry":5, "special":15, "corvette":15, "amphibious":15, "patrol":15, "cruiser":7, "destroyer":10, "battleship":5, "carrier":5, "attack-submarine":15, "missile-submarine":15, "light-fighter":30, "heavy-fighter":15, "bomber":15, "stealth-bomber":10, "recon":20, "transport":30, "drone":30}
@@ -72,8 +72,9 @@ def update():
     firstHealth = sum(firstTeamTable.values())
     secondHealth = sum(secondTeamTable.values())
 
-def error(code):
+def error(code, function):
     print(errorMessages.get(code))
+    print("Error message called by", str(function))
 
 def remove(item, list):
     global usedUnits
@@ -146,10 +147,10 @@ def fortificationReduce(structure, damage):
 
 def damage(unit, unitType, table):
     if check(unit, immobileUnits) == True or check(unit, usedUnits) == True:
-        error("available")
+        error("available", "damage")
         return
     if check(unit, deadUnits) == True:
-        error("dead")
+        error("dead", "damage")
         return
     if check(unit, table) == False: return
     basicMaximum = float(table.get(unitType)) + 1
@@ -202,14 +203,37 @@ def helpText():
     print("Unit types:")
     print(*allUnitTypes.keys(), sep = ", ")
 
-def health():
-    pass
+def kill(arguments):
+    global firstTeamTable
+    global secondTeamTable
+    unit = arguments[2]
+    if unit in firstTeamTable: table = firstTeamTable
+    elif unit in secondTeamTable: table = secondTeamTable
+    else: 
+        error("unit", "kill")
+        return
+    del table[unit]
+    append(unit, deadUnits)
+    update()
 
-def kill():
-    pass
+def health(arguments):
+    global firstTeamTable
+    global secondTeamTable
+    unit = arguments[2]
+    newHealth = float(arguments[3])
+    if unit in firstTeamTable: table = firstTeamTable
+    elif unit in secondTeamTable: table = secondTeamTable
+    else:
+        error("unit", "health")
+        return
+    currentHealth = table.get(unit)
+    if newHealth <= 0: kill(arguments)
+    else: table[unit] = float(newHealth)
+    update()
 
-def freeze():
-    pass
+def freeze(arguments):
+    unit = arguments[2]
+    append(unit, immobileUnits)
 
 def convert():
     pass
@@ -316,10 +340,10 @@ def shell(team, targetTeam, teamTable, targetTeamTable, teamFlyingTable, targetT
     elif parsedCommand[1] == "details": details()
     elif parsedCommand[1] == "quit": quitGame()
     elif parsedCommand[1] == "help": helpText()
-    elif parsedCommand[1] == "health": pass
-    elif parsedCommand[1] == "kill": pass
-    elif parsedCommand[1] == "man": pass
-    elif parsedCommand[1] == "freeze": pass
+    elif parsedCommand[1] == "health": health(parsedCommand)
+    elif parsedCommand[1] == "kill": kill(parsedCommand)
+    elif parsedCommand[1] == "man": man(parsedCommand)
+    elif parsedCommand[1] == "freeze": freeze(parsedCommand)
     elif parsedCommand[1] == "convert": pass
     elif parsedCommand[1] == "disable": pass
     elif parsedCommand[1] == "merge": pass
