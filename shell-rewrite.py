8eebe469797ec0cea12f68b5be749e8bc5269631
Gavin Faulkner <82196920+gtfmadrigal@users.noreply.github.com>
2021-06-21 20:58:57 -0400

    v3.0.11

--- shell-rewrite.py
+++ shell-rewrite.py
@@ -10,7 +10,7 @@ campaign = "Normandy"
 firstHealth = sum(firstTeamTable.values())
 secondHealth = sum(secondTeamTable.values())
 helpTextBlock = """
-Umpire commands: score, turn, details, quit, help, health, kill, freeze, convert, disable, merge, split, info, use
+Umpire commands: score, turn, details, quit, help, health, kill, freeze, convert, disable, merge, split, info, use, man
 Theater-agnostic commands: attack, move, hide, reveal, spy, fire
 Naval commands: heading, torpedo, sortie, depthcharge, board
 Army commands: build, missile
@@ -33,7 +33,7 @@ hideTable = ["infantry", "engineers", "mechanized", "light-artillery", "med-arti
 hideableTerrain = ["forest", "swamp", "ocean"]
 
 # Dictionaries
-errorMessages = {"argument":"Bad argument for command. Type 'man [command] for details.", "team":"That unit belongs to the wrong team.", "available":"That unit is currently unavailable.", "function":"That function is unavailable to this unit.", "heading":"Unit cannot exceed its maximum heading change.", "dead":"Unit is dead.", "type":"No such unit type.", "unit":"No such unit.", "hidden":"Unit is already hidden.", "required":"Heading changes are not required for this unit.", "airborne":"Unit is not airborne.", "board":"Unit is not a boardable ship", "exists":"A unit with that name already exists.", "terrain":"The terrain does not allow for that function to be called."}
+errorMessages = {"argument":"Bad argument for command. Type 'man [command] for details.", "team":"That unit belongs to the wrong team.", "available":"That unit is currently unavailable.", "function":"That function is unavailable to this unit.", "heading":"Unit cannot exceed its maximum heading change.", "dead":"Unit is dead.", "type":"No such unit type.", "unit":"No such unit.", "hidden":"Unit is already hidden.", "required":"Heading changes are not required for this unit.", "airborne":"Unit is not airborne.", "board":"Unit is not a boardable ship", "exists":"A unit with that name already exists.", "terrain":"The terrain does not allow for that function to be called.", "command":"Unknown command. Type 'help' to see a list of commands, or 'man [command]' to see how to use a particular command."}
 dividedTable = {}
 healthTable = {"infantry":4, "engineers":4, "mechanized":6, "light-artillery":8, "med-artillery":9, "heavy-artillery":10, "light-cavalry":12, "med-cavalry":14, "heavy-cavalry":16, "special":20, "corvette":4, "amphibious":4, "patrol":2, "cruiser":10, "destroyer":8, "battleship":12, "carrier":16, "attack-submarine":1, "missile-submarine":1, "light-fighter":4, "heavy-fighter":8, "bomber":12, "stealth-bomber":10, "recon":4, "transport":12, "drone":4}
 movementTable = {"infantry":10, "engineers":10, "mechanized":15, "light-artillery":10, "med-artillery":7, "heavy-artillery":5, "light-cavalry":10, "med-cavalry":7, "heavy-cavalry":5, "special":15, "corvette":15, "amphibious":15, "patrol":15, "cruiser":7, "destroyer":10, "battleship":5, "carrier":5, "attack-submarine":15, "missile-submarine":15, "light-fighter":30, "heavy-fighter":15, "bomber":15, "stealth-bomber":10, "recon":20, "transport":30, "drone":30}
@@ -55,8 +55,8 @@ kamikazeTable = {"light-fighter":6, "heavy-fighter":8}
 moveFireTable = {"infantry":1, "engineers":1, "mechanized":1, "light-cavalry":1, "med-cavalry":1, "heavy-cavalry":1, "special":1, "corvette":1, "amphibious":1, "patrol":1, "cruiser":1, "destroyer":1, "battleship":1, "carrier":1, "light-fighter":1, "heavy-fighter":1, "bomber":1, "stealth-bomber":1, "recon":4, "transport":1, "drone":1}
 bombTable = {"bomber":8, "stealth-bomber":6, "drone":10}
 flyTable = {"light-fighter":1, "heavy-fighter":1, "bomber":1, "stealth-bomber":1, "recon":1, "transport":1, "drone":1}
-manTable = {}
 structureTable = {}
+manPages = {"score":"'score'", "turn":"'turn'", "details":"'details'", "quit":"'quit'", "help":"'help'", "health":"'health [unit]'", "kill":"'kill [unit]'", "convert":"'convert [unit]'", "disable":"'disable [unit]'", "merge":"'merge [unit1] [unit2] ... > [unit]'", "split":"'split [unit] > [unit1] [unit2] ...'", "info":"'info [unit]'", "use":"'use [unit]'", "man":"'man [command]'"}
 
 # Initialization work
 loadGame()
@@ -396,11 +396,13 @@ def split(arguments, teamTable):
     if originalUnit in usedUnits: remove(originalUnit, usedUnits)
     if originalUnit in hiddenUnits: reveal(originalUnit)       
 
-def info():
-    pass
-
-def man():
-    pass
+def man(arguments):
+    command = arguments[2]
+    if not command in manPages:
+        error("command", "man")
+        print("Type 'help' for a list of commands.")
+        return
+    print(manPages.get(command))
 
 # Theater-agnostic functions
 def attack():
@@ -504,7 +506,7 @@ def shell(team, targetTeam, teamTable, targetTeamTable, teamFlyingTable, targetT
     elif parsedCommand[1] == "build": pass
     elif parsedCommand[1] == "missile": pass
     else:
-        print("Unknown command. Type 'help' to see a list of commands, or 'man [command]' to see how to use a particular command.")
+        error("command", "shell")
         return
     commandNumber = commandNumber + 1
 
