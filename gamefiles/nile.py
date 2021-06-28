81bbab84ee2e1cda37b13763b53d2dd9cda5a1f3
Gavin Faulkner <82196920+gtfmadrigal@users.noreply.github.com>
2021-06-28 16:44:00 -0400

    v4.0-pre3.5

--- gamefiles/nile.py
+++ gamefiles/nile.py
@@ -1,23 +1,14 @@
 firstTeam = "British"
 secondTeam = "French"
-unitTypeTable = {"carrier":"navy", "battleship":"navy", "cruiser":"navy", "destroyer":"navy", "corvette":"navy", "amphibious":"navy", "patrol":"navy", "attack-submarine":"navy", "missile-submarine":"navy"}
+campaign = "Nile"
+allUnitTypes = {"carrier":"carrier", "battleship":"battleship", "cruiser":"cruiser", "destroyer":"destroyer", "corvette":"corvette", "amphibious":"amphibious", "patrol":"patrol", "attack-submarine":"attack-submarine", "missile-submarine":"missile-submarine"}
 unitTable = {"culloden":"carrier", "leander":"carrier", "swiftsure":"battleship", "alexander":"battleship", "majestic":"cruiser", "bellerophon":"cruiser", "defence":"destroyer", "minotaur":"destroyer", "vanguard":"corvette", "orion":"corvette", "audacious":"amphibious", "zealous":"amphibious", "goliath":"patrol", "theseus":"patrol", "leviathan":"attack-submarine", "victory":"missile-submarine", "guerrier":"carrier", "conquerant":"carrier", "sparta":"battleship", "aquilon":"battleship", "souverain":"cruiser", "franklin":"cruiser", "orient":"destroyer", "tonnant":"destroyer", "heureux":"corvette", "artemise":"corvette", "mercure":"amphibious", "guillame":"amphibious", "diane":"patrol", "justice":"patrol", "timoleon":"attack-submarine", "genereux":"missile-submarine"}
 firstTeamTable = {"culloden":16, "leander":16, "swiftsure":12, "alexander":12, "majestic":10, "bellerophon":10, "defence":8, "minotaur":8, "vanguard":4, "orion":4, "audacious":4, "zealous":4, "goliath":2, "theseus":2, "leviathan":1, "victory":1}
 secondTeamTable = {"guerrier":16, "conquerant":16, "sparta":12, "aquilon":12, "souverain":10, "franklin":10, "orient":8, "tonnant":8, "heureux":4, "artemise":4, "mercure":4, "guillame":4, "diane":2, "justice":2, "timoleon":1, "genereux":1}
 firstHealthTotal = 114
 secondHealthTotal = 114
-firstHealth = sum(firstTeamTable.values())
-secondHealth = sum(secondTeamTable.values())
-healthTable = {"attack-submarine":1, "missile-submarine":1, "patrol":2, "corvette":4, "amphibious":4, "destroyer":8, "cruiser":10, "battleship":12, "carrier":16}
-movementTable = {"carrier":5, "battleship":5, "cruiser":7, "destroyer":10, "corvette":15, "amphibious":15, "patrol":15, "attack-submarine":15, "missile-submarine":15}
-attackTable = {"amphibious":4, "patrol":4, "corvette":6, "destroyer":8, "carrier":12, "cruiser":16, "battleship":24}
-sortieTable = {"carrier":8}
-fireTable = {"battleship":8, "missile-submarine":16}
-sortieDefenseTable = {"carrier":8}
-headingTable = {"carrier":1, "battleship":1, "cruiser":1, "destroyer":1, "corvette":1, "amphibious":1, "patrol":1}
-spyTable = {"amphibious":6, "patrol":6, "corvette":6}
-torpedoTable = {"attack-submarine":6, "missile-submarine":6}
-hideTable = {"attack-submarine":1, "missile-submarine":1}
-buildTable = {}
-moveFireTable = {"carrier":1, "battleship":1, "cruiser":1, "destroyer":1, "corvette":1, "amphibious":1, "patrol":1}
-depthchargeTable = {"cruiser":6, "destroyer":6, "corvette":6, "carrier":6, "battleship":6}
+fogOfWar = 1
+locationTable = {}
+
+def loadGame():
+    pass
